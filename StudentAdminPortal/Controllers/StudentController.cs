690a1dd3c8396039d34c076ae866fc4b8981f536
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-23 16:41:01 +0530

    Add StudentController with Index action
    - Added StudentController to manage student-related views.
    - Implemented Index action to render the default view for students.

---
+++ StudentAdminPortal/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAdminPortal.Controllers
+{
+    // Controller responsible for handling student-related requests
+    public class StudentController : Controller
+    {
+        // Action method that returns the default view for students
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
