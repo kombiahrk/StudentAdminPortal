c7cb6ce9686427769db95fc537dee0b08e46c088
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-23 12:46:34 +0530

    Add Student entity to StudentAdminPortal
    - Added Student class with properties:
      - Id: Unique identifier for the student.
      - Name: Required name of the student.
      - Email: Required email address of the student.
      - Phone: Optional phone number of the student.
      - Subscribed: Indicates if the student is subscribed to notifications.

---
+++ StudentAdminPortal/Models/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentAdminPortal.Models.Entities
+{
+    public class Student
+    {
+        // Unique identifier for the student
+        public Guid Id { get; set; }
+
+        // The name of the student, required field
+        public required string Name { get; set; }
+
+        // The email address of the student, required field
+        public required string Email { get; set; }
+
+        // The phone number of the student, optional field
+        public string? Phone { get; set; }
+
+        // Indicates if the student is subscribed to notifications
+        public bool Subscribed { get; set; }
+    }
+}
